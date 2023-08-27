bbe24de880e40742fd8289e551b20f22baae4c7f
Amul <amul2mohite@gmail.com>
2023-08-27 16:38:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+ const jokeEl=document.querySelector('#joke')
+ const jokebtn=document.querySelector('.btn')
+
+const generateJoke=()=>{
+    const xhr=new XMLHttpRequest()
+
+    xhr.open('GET','https://api.chucknorris.io/jokes/random')
+
+    xhr.onreadystatechange=function(){
+        if(this.readyState===4){
+            if(this.status===200){
+                jokeEl.innerHTML=JSON.parse(this.responseText).value
+            }else{
+                jokeEl.innerHTML="Something Went wrong"
+            }
+        }
+
+    }
+    xhr.send()
+}
+
+ 
+ jokebtn.addEventListener('click',generateJoke)
+ document.addEventListener('DOMContentLoaded',generateJoke)
